b0155e07bd868d0a4256c3dfd8e0c14f9643505c
unknown <yilmz.fikret@gmail.com>
2019-10-17 17:57:45 +0200

    18.Hafta odev

---
+++ ceviri2.py
@@ -0,0 +1,162 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'kullaniciKaydi2.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.0
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(928, 737)
+        font = QtGui.QFont()
+        font.setFamily("MS Sans Serif")
+        Form.setFont(font)
+        Form.setStyleSheet("background-color: rgb(255, 255, 127);\n"
+"selection-color: rgb(255, 170, 255);\n"
+"")
+        self.radioButton = QtWidgets.QRadioButton(Form)
+        self.radioButton.setGeometry(QtCore.QRect(260, 610, 151, 21))
+        self.radioButton.setObjectName("radioButton")
+        self.radioButton_2 = QtWidgets.QRadioButton(Form)
+        self.radioButton_2.setGeometry(QtCore.QRect(480, 610, 181, 20))
+        font = QtGui.QFont()
+        font.setUnderline(True)
+        self.radioButton_2.setFont(font)
+        self.radioButton_2.setObjectName("radioButton_2")
+        self.textBrowser = QtWidgets.QTextBrowser(Form)
+        self.textBrowser.setGeometry(QtCore.QRect(260, 480, 401, 121))
+        self.textBrowser.setObjectName("textBrowser")
+        self.pushButton = QtWidgets.QPushButton(Form)
+        self.pushButton.setGeometry(QtCore.QRect(260, 650, 161, 23))
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton_2 = QtWidgets.QPushButton(Form)
+        self.pushButton_2.setGeometry(QtCore.QRect(480, 650, 171, 23))
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.pushButton_3 = QtWidgets.QPushButton(Form)
+        self.pushButton_3.setGeometry(QtCore.QRect(0, 0, 101, 31))
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.pushButton_4 = QtWidgets.QPushButton(Form)
+        self.pushButton_4.setGeometry(QtCore.QRect(0, 52, 101, 31))
+        self.pushButton_4.setObjectName("pushButton_4")
+        self.pushButton_5 = QtWidgets.QPushButton(Form)
+        self.pushButton_5.setGeometry(QtCore.QRect(0, 110, 101, 31))
+        self.pushButton_5.setObjectName("pushButton_5")
+        self.pushButton_6 = QtWidgets.QPushButton(Form)
+        self.pushButton_6.setGeometry(QtCore.QRect(0, 170, 101, 31))
+        self.pushButton_6.setObjectName("pushButton_6")
+        self.pushButton_7 = QtWidgets.QPushButton(Form)
+        self.pushButton_7.setGeometry(QtCore.QRect(0, 220, 101, 31))
+        self.pushButton_7.setObjectName("pushButton_7")
+        self.pushButton_8 = QtWidgets.QPushButton(Form)
+        self.pushButton_8.setGeometry(QtCore.QRect(320, 220, 101, 31))
+        self.pushButton_8.setObjectName("pushButton_8")
+        self.checkBox = QtWidgets.QCheckBox(Form)
+        self.checkBox.setGeometry(QtCore.QRect(140, 110, 101, 31))
+        self.checkBox.setObjectName("checkBox")
+        self.checkBox_2 = QtWidgets.QCheckBox(Form)
+        self.checkBox_2.setGeometry(QtCore.QRect(290, 110, 101, 31))
+        self.checkBox_2.setObjectName("checkBox_2")
+        self.checkBox_3 = QtWidgets.QCheckBox(Form)
+        self.checkBox_3.setGeometry(QtCore.QRect(140, 170, 101, 31))
+        self.checkBox_3.setObjectName("checkBox_3")
+        self.checkBox_4 = QtWidgets.QCheckBox(Form)
+        self.checkBox_4.setGeometry(QtCore.QRect(290, 170, 101, 31))
+        self.checkBox_4.setObjectName("checkBox_4")
+        self.checkBox_5 = QtWidgets.QCheckBox(Form)
+        self.checkBox_5.setGeometry(QtCore.QRect(430, 170, 101, 31))
+        self.checkBox_5.setObjectName("checkBox_5")
+        self.checkBox_6 = QtWidgets.QCheckBox(Form)
+        self.checkBox_6.setGeometry(QtCore.QRect(560, 170, 101, 31))
+        self.checkBox_6.setObjectName("checkBox_6")
+        self.textEdit = QtWidgets.QTextEdit(Form)
+        self.textEdit.setGeometry(QtCore.QRect(650, 170, 181, 41))
+        self.textEdit.setObjectName("textEdit")
+        self.plainTextEdit = QtWidgets.QPlainTextEdit(Form)
+        self.plainTextEdit.setGeometry(QtCore.QRect(130, 220, 141, 31))
+        self.plainTextEdit.setObjectName("plainTextEdit")
+        self.plainTextEdit_2 = QtWidgets.QPlainTextEdit(Form)
+        self.plainTextEdit_2.setGeometry(QtCore.QRect(460, 220, 141, 31))
+        self.plainTextEdit_2.setObjectName("plainTextEdit_2")
+        self.pushButton_9 = QtWidgets.QPushButton(Form)
+        self.pushButton_9.setGeometry(QtCore.QRect(0, 270, 101, 31))
+        self.pushButton_9.setObjectName("pushButton_9")
+        self.lineEdit = QtWidgets.QLineEdit(Form)
+        self.lineEdit.setGeometry(QtCore.QRect(130, 270, 113, 31))
+        self.lineEdit.setObjectName("lineEdit")
+        self.pushButton_10 = QtWidgets.QPushButton(Form)
+        self.pushButton_10.setGeometry(QtCore.QRect(320, 270, 101, 31))
+        self.pushButton_10.setObjectName("pushButton_10")
+        self.lineEdit_2 = QtWidgets.QLineEdit(Form)
+        self.lineEdit_2.setGeometry(QtCore.QRect(460, 270, 141, 31))
+        self.lineEdit_2.setObjectName("lineEdit_2")
+        self.pushButton_11 = QtWidgets.QPushButton(Form)
+        self.pushButton_11.setGeometry(QtCore.QRect(260, 450, 401, 31))
+        self.pushButton_11.setObjectName("pushButton_11")
+        self.plainTextEdit_3 = QtWidgets.QPlainTextEdit(Form)
+        self.plainTextEdit_3.setGeometry(QtCore.QRect(140, 0, 141, 31))
+        self.plainTextEdit_3.setObjectName("plainTextEdit_3")
+        self.plainTextEdit_4 = QtWidgets.QPlainTextEdit(Form)
+        self.plainTextEdit_4.setGeometry(QtCore.QRect(140, 50, 141, 31))
+        self.plainTextEdit_4.setObjectName("plainTextEdit_4")
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Kayit Formu"))
+        self.radioButton.setText(_translate("Form", "Okudum Onayliyorum"))
+        self.radioButton_2.setText(_translate("Form", "Okudum Onaylamiyorum"))
+        self.textBrowser.setHtml(_translate("Form", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\" margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'MS Sans Serif\';\">Uyelik Sartlari Lutfen Sartlari okuyup onayladiktan sonra Kabul Ederek Bir Sonraki Asamaya Geciniz.</span></p>\n"
+"<p style=\" margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p>\n"
+"<p style=\" margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'MS Sans Serif\';\">1)dssdghjkhgsdgdfhjdggggggggggggggggggggggggggggggggggggggggggggggghhjgh</span></p>\n"
+"<p style=\" margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'MS Sans Serif\';\">2)jghjghjgktertyfhfgnghjggggggggg</span></p>\n"
+"<p style=\" margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'MS Sans Serif\';\">3)ytruryktuktjyyhyhy</span></p>\n"
+"<p style=\" margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'MS Sans Serif\';\">4)jgkgjkgjkgjkdera</span></p>\n"
+"<p style=\" margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'MS Sans Serif\';\">5)hyjtuioyipuiåökljkhgdftryuiolökm,nhgdg</span></p></body></html>"))
+        self.pushButton.setText(_translate("Form", "Kayit OL"))
+        self.pushButton_2.setText(_translate("Form", "Iptal"))
+        self.pushButton_3.setText(_translate("Form", "Adi"))
+        self.pushButton_4.setText(_translate("Form", "Soyadi"))
+        self.pushButton_5.setText(_translate("Form", "Cinsiyet"))
+        self.pushButton_6.setText(_translate("Form", "Medeni Durum"))
+        self.pushButton_7.setText(_translate("Form", "Mail"))
+        self.pushButton_8.setText(_translate("Form", "Mail Tekrar"))
+        self.checkBox.setText(_translate("Form", "Erkek"))
+        self.checkBox_2.setText(_translate("Form", "Bayan"))
+        self.checkBox_3.setText(_translate("Form", "Evli"))
+        self.checkBox_4.setText(_translate("Form", "Bekar"))
+        self.checkBox_5.setText(_translate("Form", "Bosanmis"))
+        self.checkBox_6.setText(_translate("Form", "Diger"))
+        self.textEdit.setHtml(_translate("Form", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'MS Sans Serif\';\">Ek Aciklama:</span></p></body></html>"))
+        self.plainTextEdit.setPlainText(_translate("Form", "Mail Giriniz:"))
+        self.plainTextEdit_2.setPlainText(_translate("Form", "Mail Giriniz:"))
+        self.pushButton_9.setText(_translate("Form", "Parola Tekrar"))
+        self.pushButton_10.setText(_translate("Form", "Parola Tekrar"))
+        self.pushButton_11.setText(_translate("Form", "Uyelik Sartlari"))
+        self.plainTextEdit_3.setPlainText(_translate("Form", "Adinizi Giriniz:"))
+        self.plainTextEdit_4.setPlainText(_translate("Form", "Soyadinizil Giriniz:"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Form = QtWidgets.QWidget()
+    ui = Ui_Form()
+    ui.setupUi(Form)
+    Form.show()
+    sys.exit(app.exec_())
